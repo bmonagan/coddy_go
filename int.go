769709996898d496776116ea79a3d3ea006575f2
bmonagan <bernardmonagan@gmail.com>
2026-02-10 21:40:49 -0500

    int

---
+++ int.go
@@ -0,0 +1,17 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+	// Create your integer variables here
+	age := 25
+	year := 2023
+	temperature := -5
+	
+	// Print the variables on separate lines
+	fmt.Println(age)
+	fmt.Println(year)
+	fmt.Println(temperature)
+	
+}

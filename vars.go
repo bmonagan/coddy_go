599088cd7e9258178b7fa2cab5655a8492ef0011
bmonagan <bernardmonagan@gmail.com>
2026-02-10 21:38:13 -0500

    vars

---
+++ vars.go
@@ -0,0 +1,15 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: Declare a variable named 'name' as a string with value "Gopher" using var
+    var name string = "Gopher"
+    
+    // TODO: Declare a variable named 'age' as an int with value 5 using var
+    var age int = 5
+    
+    // Don't modify the code below
+    fmt.Println("My name is", name, "and I am", age, "years old.")
+}

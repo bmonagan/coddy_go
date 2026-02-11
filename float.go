8059251c350c03fc4da58b451a316d2de0785bed
bmonagan <bernardmonagan@gmail.com>
2026-02-10 21:41:27 -0500

    float

---
+++ float.go
@@ -0,0 +1,20 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+	// Some examples of floating-point numbers
+	pi := 3.14159
+	temperature := 72.5
+
+	// TODO: Declare a new float64 variable named 'height' with the value 5.9
+	height := 5.9
+		
+	// Print the existing variables
+	fmt.Println("Pi approximately equals:", pi)
+	fmt.Println("The temperature is:", temperature)
+	
+	// TODO: Print your new 'height' variable with a descriptive message
+	fmt.Println("The height is:", height)
+}

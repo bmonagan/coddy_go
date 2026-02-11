fe1f6ed081ba8bdcf4f93446d9a6eee7865ba835
bmonagan <bernardmonagan@gmail.com>
2026-02-10 21:39:12 -0500

    type inf

---
+++ type_inf.go
@@ -0,0 +1,22 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+    // Variables declared with explicit types
+    var name string = "Gopher"
+    var age int = 5
+    
+    // TODO: Declare a variable called 'language' with the value "Go" using := syntax
+    language := "Go"
+    
+    // TODO: Declare a variable called 'version' with the value 1.18 using := syntax
+    version := 1.18
+    
+    // Print all variables
+    fmt.Println("Name:", name)
+    fmt.Println("Age:", age)
+    fmt.Println("Language:", language)
+    fmt.Println("Version:", version)
+}

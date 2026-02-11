3c5f0168fcc1907f077c71292a7f1ec1ee42ab21
bmonagan <bernardmonagan@gmail.com>
2026-02-10 21:37:34 -0500

    comments.go

---
+++ comments.go
@@ -0,0 +1,14 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+    // TODO: Add a single-line comment here explaining that this program displays a welcome message
+    // displays a welcome message
+
+    // TODO: Add a multi-line comment here explaining that we're using fmt.Println to display text
+    /* multi line 
+        junka */
+    fmt.Println("Welcome to Go programming!")
+}

af85745878b6983be183e9d8a7a529896a5bbb9d
bmonagan <bernardmonagan@gmail.com>
2026-02-11 21:28:30 -0500

    consts

---
+++ const.go
@@ -0,0 +1,12 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: Declare a constant named maxStudents with value 30
+	const maxStudents = 30
+	
+	// Print the constant
+	fmt.Println("Maximum number of students allowed:", maxStudents)
+}

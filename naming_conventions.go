6970bd19d3c83a1f9d255907c53fee74cebfd0a6
bmonagan <bernardmonagan@gmail.com>
2026-02-11 21:30:06 -0500

    naming

---
+++ naming_conventions.go
@@ -0,0 +1,18 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+	// TODO: Fix these variable names to follow Go naming conventions
+	var userName string = "John"
+	var userAge int = 25
+	var isActive bool = true
+	var firstLogin string = "2023-01-15"
+
+	// TODO: Fix these variable names to follow Go naming conventions
+	fmt.Println("User:", userName)
+	fmt.Println("Age:", userAge)
+	fmt.Println("Active:", isActive)
+	fmt.Println("First Login:", firstLogin)
+}

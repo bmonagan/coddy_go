2be6c1b076f92baa394848c5eac3049ffb9af850
bmonagan <bernardmonagan@gmail.com>
2026-02-11 21:24:09 -0500

    bool.go

---
+++ bool.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Two boolean variables are already defined
+	likesGo := true
+	firstProgram := false
+	
+	// TODO: Create a boolean variable called 'isLearning' and set it to true
+	isLearning := true
+	
+	// TODO: Create a boolean variable called 'isExpert' and set it to false
+	isExpert := false
+	// Print all the boolean variables
+	fmt.Println("Likes Go:", likesGo)
+	fmt.Println("First program:", firstProgram)
+	fmt.Println("Is learning:", isLearning)
+	fmt.Println("Is expert:", isExpert)
+}

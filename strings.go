9722ed5fcac371f850bc991fbe41d8ea39628789
bmonagan <bernardmonagan@gmail.com>
2026-02-11 21:25:17 -0500

    string

---
+++ strings.go
@@ -0,0 +1,18 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+	// These variables are already defined for you
+	firstName := "Alex"
+	lastName := "Smith"
+	
+	// TODO: Create a greeting string variable with the value "Hello, my name is "
+	greeting := "Hello, my name is "
+	// TODO: Print all three strings (greeting, firstName, and lastName) on separate lines
+	fmt.Println(greeting)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	
+}

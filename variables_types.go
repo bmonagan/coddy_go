8418ed8cf0a0fa15941fbeb708f44bbc06c3e83f
bmonagan <bernardmonagan@gmail.com>
2026-02-14 23:33:18 -0500

    variables and types

---
+++ variables_types.go
@@ -0,0 +1,18 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+  // Define given variables
+  productName := "Coffee Mug"
+
+  // Define a new integer variable named 'quantity' with value 5
+  quantity := 5
+  
+  // Print the product name with a label
+  fmt.Println("Product:", productName)
+  
+  // Print the quantity with a label
+  fmt.Println("Quantity:", quantity)
+}

ec992213f2932115ec07a3faa6597a540fa269e6
bmonagan <bernardmonagan@gmail.com>
2026-02-15 23:06:23 -0500

    arithmaetic_operators.go

---
+++ arithmaetic_operators.go
@@ -0,0 +1,17 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+	// These variables are already defined for you
+	num1 := 5
+	num2 := 7
+	result := 0
+	
+	// TODO: Calculate (num1 + num2) * 2 and store it in the result variable
+	result = (num1 + num2) * 2 
+	
+	// This will print the result
+	fmt.Println("The result is:", result)
+}

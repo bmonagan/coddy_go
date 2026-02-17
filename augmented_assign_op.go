85311ce1dcdbe31c8144383ea4975b8990154d8f
bmonagan <bernardmonagan@gmail.com>
2026-02-16 22:04:50 -0500

    augmented assign op

---
+++ augmented_assign_op.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Starting score
+	score := 10
+
+	// TODO: Use augmented assignment (+=) to increase score by 5
+	score += 5
+
+	// Print the final score
+	fmt.Println("Final score:", score)
+}

680b3e7cc5b89d4c51a6fa325760af68d7bd8748
bmonagan <bernardmonagan@gmail.com>
2026-02-16 22:04:17 -0500

    assignment operator

---
+++ assignment_operator.go
@@ -0,0 +1,15 @@
+package coddygo
+package main
+
+import "fmt"
+
+func main() {
+	// This variable is already declared
+	favoriteColor := "red"
+	
+	// TODO: Use the assignment operator (=) to change favoriteColor to "blue"
+	favoriteColor = "blue"
+	
+	// This will print the value of favoriteColor
+	fmt.Println("My favorite color is:", favoriteColor)
+}

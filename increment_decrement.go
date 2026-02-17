8b2662c9f7c4444c7e431fec99ea83324a453c43
bmonagan <bernardmonagan@gmail.com>
2026-02-16 22:06:02 -0500

    increment decrement operator

---
+++ increment_decrement.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Starting value for our counter
+	counter := 5
+
+	// TODO: Increment counter twice using the ++ operator
+	counter++
+	counter++
+
+	// TODO: Decrement counter once using the -- operator
+	counter--
+	// Print the final value
+	fmt.Println("Final counter value:", counter)
+}
